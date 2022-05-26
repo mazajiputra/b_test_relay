c289504cd8d75ee8fff541d4e04980c5b3f01faa
mazajiputra <mazajiputra@gmail.com>
2022-05-26 23:54:45 +0700

    Update example_relay.py

--- example_relay.py
+++ example_relay.py
@@ -9,20 +9,25 @@ GPIO.setup(12, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
-
-while (True):
-    
-    # Turn all relays ON
-    GPIO.output(12, GPIO.HIGH)
-    GPIO.output(16, GPIO.HIGH)
-    GPIO.output(20, GPIO.HIGH)
-    GPIO.output(21, GPIO.HIGH)
-    # Sleep for 5 seconds
-    # sleep(5) 
-    # Turn all relays OFF
-    # GPIO.output(18, GPIO.LOW)
-    # GPIO.output(23, GPIO.LOW)
-    # GPIO.output(24, GPIO.LOW)
-    # GPIO.output(25, GPIO.LOW)   
-    # Sleep for 5 seconds
+# Disable Warnings
+GPIO.setwarnings(False)
+try:
+    while (True):
+        
+        # Turn all relays ON
+        GPIO.output(12, GPIO.HIGH)
+        GPIO.output(16, GPIO.HIGH)
+        GPIO.output(20, GPIO.HIGH)
+        GPIO.output(21, GPIO.HIGH)
+        # Sleep for 5 seconds
+        # sleep(5) 
+        # Turn all relays OFF
+except KeyboardInterrupt:
+# catches the ctrl-c command, breaks the loop above 
+# and turns the relays off
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(23, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+    GPIO.output(25, GPIO.LOW)   
+    #Sleep for 5 seconds
     # sleep(5)
