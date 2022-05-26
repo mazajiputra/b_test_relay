a47bd12e2f8fc853a057984bf16e68eb84519023
mazajiputra <mazajiputra@gmail.com>
2022-05-26 23:08:34 +0700

    Update example_relay.py

--- example_relay.py
+++ example_relay.py
@@ -1,13 +1,28 @@
-#!/usr/bin/python
-import RPi.GPIO as GPIO 
-from time import sleep 
-GPIO.setmode(GPIO.BCM) 
-Relay1_GPIO = 12
-Relay2_GPIO = 16 
-Relay3_GPIO = 20
-Relay4_GPIO = 21
-GPIO.output(Relay1_GPIO, GPIO.LOW)  
-GPIO.output(Relay2_GPIO, GPIO.LOW) 
-GPIO.output(Relay3_GPIO, GPIO.LOW)  
-GPIO.output(Relay4_GPIO, GPIO.LOW)  
-sleep(1) 
+import RPi.GPIO as GPIO
+from time import sleep
+
+# The script as below using BCM GPIO 00..nn numbers
+GPIO.setmode(GPIO.BCM)
+
+# Set relay pins as output
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+while (True):
+    
+    # Turn all relays ON
+    GPIO.output(12, GPIO.HIGH)
+    GPIO.output(16, GPIO.HIGH)
+    GPIO.output(20, GPIO.HIGH)
+    GPIO.output(21, GPIO.HIGH)
+    # Sleep for 5 seconds
+    # sleep(5) 
+    # Turn all relays OFF
+    # GPIO.output(18, GPIO.LOW)
+    # GPIO.output(23, GPIO.LOW)
+    # GPIO.output(24, GPIO.LOW)
+    # GPIO.output(25, GPIO.LOW)   
+    # Sleep for 5 seconds
+    # sleep(5)
